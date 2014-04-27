13444420db542088f3f69e18c52d56fbb28ec0b8
kkabdol <kkabdol@gmail.com>
2014-04-27 17:38:50 +0900

    4th one game a week started
    Roman Roulette from ACM problem

---
+++ Assets/GameController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour
+{
+	public int numOfVictims = 3;
+	public GameObject victimPrefab;
+	public GameObject killSign;
+
+	private List<GameObject> victimList = new List<GameObject> ();
+	private int firstVictimIndex = 0;
+
+	void Awake ()
+	{
+		GenerateVictims ();
+		ChooseFirstVictim ();
+	}
+		 
+	void GenerateVictims ()
+	{
+		victimList.Clear ();
+
+		float deltaRotY = 360f / numOfVictims;
+		for (int i=0; i<numOfVictims; ++i) {
+			Quaternion rot = Quaternion.Euler (0f, deltaRotY * i, 0f);
+			GameObject victim = Instantiate (victimPrefab, Vector3.zero, rot) as GameObject;
+			victimList.Add (victim);
+		}
+	}
+
+	void ChooseFirstVictim ()
+	{
+		firstVictimIndex = Random.Range (0, victimList.Count);
+		killSign.transform.position = victimList [firstVictimIndex].GetComponent<Victim> ().victimBody.transform.position;
+	}
+}
