8ea78baccba1f5aa7872abe5e284422b5ea5be3b
kkabdol <kkabdol@gmail.com>
2014-05-04 17:54:30 +0900

    Finished

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,84 +2,91 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
+public enum GameState
+{
+	Ready,
+	ShowSign,
+	Choose,
+	Execution,
+	Result,
+};
+
 public class GameController : MonoBehaviour
 {
-	public int numOfVictims = 3;
+	public GameState curState = GameState.Ready;
+	public int numberOfVictims = 3;
 	public GameObject victimPrefab;
 	public GameObject killSign;
+	public AudioSource rifleAudioSource;
+	public Transform riflePivot;
+	public AudioClip clipYouKilledMe;
+	public AudioClip clipDying;
+
+	public AudioListener listener;
 
 	private List<GameObject> victimList = new List<GameObject> ();
 	private int curVictimIndex = 0;
-	private bool hasVictimChoosed = false;
 	private GameObject chosenVictim;
-
-	public const float delay = 1f;
-	private float elapsedTime = 0f;
-	private int count = 0;
-
 
 	void Awake ()
 	{
 		Reset ();
 	}
 
-	void Update ()
-	{
-		if (hasVictimChoosed == true) {
-			if (victimList.Count > 1) {
-				elapsedTime += Time.deltaTime;
-
-				if (elapsedTime > delay) {
-					elapsedTime = 0;
-
-					if (count > 2) {
-						count = 0;
-
-						GameObject killed = victimList [curVictimIndex];
-
-						if (killed == chosenVictim) {
-							Reset ();
-						} else {
-							victimList.RemoveAt (curVictimIndex);
-							--curVictimIndex;
-
-							Destroy (killed);
-						}
-					} else {
-						++count;
-						curVictimIndex = (curVictimIndex + 1) % victimList.Count;
-						MoveSign (victimList [curVictimIndex]);
-					}
-				}
-			} else {
-				++numOfVictims;
-				Reset ();
-			}
-		}
-	}
+	#region ready state
 
 	void Reset ()
 	{
-		hasVictimChoosed = false;
-		elapsedTime = 0f;
-		count = 3;
+		ChangeGameState (GameState.Ready);
 
-		GenerateVictims ();
-		ChooseFirstVictim ();
+		HideSign ();
+		ClearVictimList ();
+		StartCoroutine (GenerateVictims ());
+
 	}
-		 
-	void GenerateVictims ()
+
+	void ChangeGameState (GameState newState)
+	{
+		curState = newState;
+
+		switch (newState) {
+		case GameState.ShowSign:
+			StartCoroutine (ShowSign ());
+			break;
+		case GameState.Execution:
+			StartCoroutine (Execution ());
+			break;
+		default:
+			break;
+		}
+	}
+
+	void ClearVictimList ()
 	{
 		for (int i=0, imax=victimList.Count; i<imax; ++i) {
 			Destroy (victimList [i]);
 		}
 		victimList.Clear ();
+	}
 
-		float deltaRotY = 360f / numOfVictims;
-		for (int i=0; i<numOfVictims; ++i) {
+	IEnumerator GenerateVictims ()
+	{
+		float deltaRotY = 360f / numberOfVictims;
+		for (int i=0; i<numberOfVictims; ++i) {
 			Quaternion rot = Quaternion.Euler (0f, deltaRotY * i, 0f);
 			GameObject victim = Instantiate (victimPrefab, Vector3.zero, rot) as GameObject;
 			victimList.Add (victim);
+
+			yield return new WaitForSeconds (1f);
+		}
+
+		ChangeGameState (GameState.ShowSign);
+	}
+
+	void StartTwinklingToAllVictims ()
+	{
+		for (int i=0, imax=victimList.Count; i<imax; ++i) {
+			victimList [i].GetComponent<Victim> ().StartTwinkling ();
 		}
 	}
 
@@ -89,19 +96,104 @@ public class GameController : MonoBehaviour
 		MoveSign (victimList [curVictimIndex]);
 	}
 
+	void HideSign ()
+	{
+		killSign.SetActive (false);
+	}
+
 	void MoveSign (GameObject victim)
 	{
+		if (killSign.activeSelf == false) {
+			killSign.SetActive (true);
+		}
 		killSign.transform.position = victim.GetComponent<Victim> ().body.transform.position;
 	}
 
+	#endregion
+
+	#region choose state
+	IEnumerator ShowSign ()
+	{
+		ChooseFirstVictim ();
+		yield return new WaitForSeconds (1f);
+		
+		StartTwinklingToAllVictims ();
+		ChangeGameState (GameState.Choose);
+	}
+	#endregion
+
+	#region choose state
 	public void ChooseVictim (GameObject chosen)
 	{
 		for (int i=0, imax=victimList.Count; i<imax; ++i) {
 			if (victimList [i] == chosen) {
 				chosenVictim = chosen;
-				hasVictimChoosed = true;
+				ChangeGameState (GameState.Execution);
+				MoveAudioListener (chosen.GetComponent<Victim> ().body.transform);
 				break;
 			}
 		}
 	}
+
+	void MoveAudioListener (Transform transform)
+	{
+		listener.gameObject.transform.position = transform.position;
+		listener.gameObject.transform.rotation = transform.rotation;
+	}
+	#endregion
+
+	#region execution state
+
+	void ShowAllVictims ()
+	{
+		for (int i=0, imax=victimList.Count; i<imax; ++i) {
+			Victim v = victimList [i].GetComponent<Victim> ();
+			v.StopTwinkling ();
+			v.ShowBody ();
+		}
+	}
+
+	IEnumerator Execution ()
+	{
+		ShowAllVictims ();
+
+		int count = 3;
+		while (victimList.Count > 1) {
+			yield return new WaitForSeconds (1f);
+
+			if (count > 2) {
+
+				PlayRifleShotOnRandomPosition ();
+				GameObject killed = victimList [curVictimIndex];
+				
+				if (killed == chosenVictim) {
+					AudioSource.PlayClipAtPoint (clipYouKilledMe, killed.GetComponent<Victim> ().body.transform.position);
+					Reset ();
+					yield break;
+				} else {
+					victimList.RemoveAt (curVictimIndex);
+					--curVictimIndex;
+
+					AudioSource.PlayClipAtPoint (clipDying, killed.GetComponent<Victim> ().body.transform.position);
+					Destroy (killed);
+				}
+
+				count = 0;
+			} else {
+				++count;
+				curVictimIndex = (curVictimIndex + 1) % victimList.Count;
+				MoveSign (victimList [curVictimIndex]);
+			}
+		}
+
+		++numberOfVictims;
+		Reset ();
+	}
+
+	void PlayRifleShotOnRandomPosition ()
+	{
+		riflePivot.Rotate (Vector3.up, Random.Range (0, 360));
+		rifleAudioSource.Play ();
+	}
+	#endregion
 }
--- Assets/Scripts/Victim.cs
+++ Assets/Scripts/Victim.cs
@@ -4,26 +4,57 @@ using System.Collections;
 public class Victim : MonoBehaviour
 {
 	public GameObject body;
+	public GameObject selectSign;
 
 	private GameController gameController;
 
 	void Awake ()
 	{
+		HideBody ();
 		gameController = GameObject.FindGameObjectWithTag (Tags.gameController).GetComponent<GameController> ();
 	}
 
 	void Update ()
 	{
-		if (Input.GetMouseButtonUp (0)) {
+		if (gameController.curState == GameState.Choose &&
+			Input.GetMouseButtonUp (0)) {
+
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit hit;
 
-			if (body.collider.Raycast (ray, out hit, float.MaxValue)) {
-				if (hit.collider == body.collider) {
-					body.renderer.material.color = Color.red;
-					gameController.ChooseVictim (this.gameObject);
+			if (selectSign.collider.Raycast (ray, out hit, float.MaxValue)) {
+				if (hit.collider == selectSign.collider) {
+					SelectSelf ();
 				}
 			}
 		}
+	}
+
+	public void StartTwinkling ()
+	{
+		selectSign.SetActive (true);
+		iTween.FadeTo (selectSign, iTween.Hash ("alpha", 0f, "looptype", iTween.LoopType.pingPong, "time", 1f));
+	}
+
+	public void StopTwinkling ()
+	{
+		iTween.Stop (selectSign);
+		selectSign.SetActive (false);
+	}
+
+	private void SelectSelf ()
+	{
+		body.renderer.material.color = Color.red;
+		gameController.ChooseVictim (this.gameObject);
+	}
+
+	public void ShowBody ()
+	{
+		body.SetActive (true);
+	}
+
+	private void HideBody ()
+	{
+		body.SetActive (false);
 	}
 }
